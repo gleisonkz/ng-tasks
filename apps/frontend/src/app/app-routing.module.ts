7f7dff5cdb47c0ac1d8f2e85148e6b3beb7d2bf8
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-21 20:59:41 -0300

    ✨ feat: dashboard component

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePage, SignInPage, SignUpPage } from '@front-end/pages';
+import {
+  DashboardPage,
+  HomePage,
+  SignInPage,
+  SignUpPage,
+} from '@front-end/pages';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePage },
   { path: 'sign-in', component: SignInPage },
   { path: 'sign-up', component: SignUpPage },
+  { path: 'dashboard', component: DashboardPage },
 ];
 
 @NgModule({
