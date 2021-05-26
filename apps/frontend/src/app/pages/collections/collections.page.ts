e5e5534e679912214a503cd5014daef51b98639c
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-26 00:27:31 -0300

    📦 refactor: add collection id

--- apps/frontend/src/app/pages/collections/collections.page.ts
+++ apps/frontend/src/app/pages/collections/collections.page.ts
@@ -6,20 +6,24 @@ export interface Task {
 }
 
 export interface Collection {
+  id: string;
   title: string;
   tasks: Task[];
 }
 
 export const COLLECTIONS_MOCK: Collection[] = [
   {
+    id: '1',
     title: 'HTML',
     tasks: [],
   },
   {
+    id: '2',
     title: 'CSS',
     tasks: [],
   },
   {
+    id: '3',
     title: 'JavaScript',
     tasks: [],
   },
